9a7356c13a9d0255ded44b8c78a4e20a43d4013f
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-05-14 00:13:33 +0200

    Numeric Value Translation

--- Analysen.py
+++ Analysen.py
@@ -5,23 +5,26 @@ import sqlite3
 import os
 import re
 
-
-
 def open_db(): #_{
 
-    global wlc, BP5_path
+    global wlc, BP5, wrd
 
     wlc_path = os.environ['github_root'] + 'Bible-Text-Sources/github.openscriptures/wlc.db'
     BP5_path = os.environ['github_root'] + 'Bible-Text-Sources/github.byztxt/BP5.db'
+    wrd_path = os.environ['github_root'] + 'Bible-Text-Sources/strongs/strongs.db'
     
     if not os.path.isfile(wlc_path):
        raise Exception(wlc_path + ' not found')
     
     if not os.path.isfile(BP5_path):
        raise Exception(BP5_path + ' not found')
+
+    if not os.path.isfile(wrd_path):
+       raise Exception(srd_path + ' not found')
     
     wlc = sqlite3.connect(wlc_path).cursor()
     PB5 = sqlite3.connect(BP5_path).cursor()
+    wrd = sqlite3.connect(wrd_path).cursor()
 #_}
 
 def init_num_values(): #_{
@@ -54,7 +57,77 @@ def init_num_values(): #_{
        'ק' : 100,
        'ר' : 200,
        'ש' : 300,
-       'ת' : 400 
+       'ת' : 400 ,
+       'Α' :   1,
+       'Β' :   2,
+       'Γ' :   3,
+       'Δ' :   4,
+       'Ε' :   5,
+       'Ζ' :   7,
+       'Η' :   8,
+       'Θ' :   9,
+       'Ι' :  10,
+       'Κ' :  20,
+       'Λ' :  30,
+       'Μ' :  40,
+       'Ν' :  50,
+       'Ξ' :  60,
+       'Ο' :  70,
+       'Π' :  80,
+       'Ρ' : 100,
+       'Σ' : 200,
+       'Τ' : 300,
+       'Υ' : 400,
+       'Φ' : 500,
+       'Χ' : 600,
+       'Ψ' : 700,
+       'Ω' : 800,
+       'α' :   1,
+       'β' :   2,
+       'γ' :   3,
+       'δ' :   4,
+       'ε' :   5,
+       'ζ' :   7,
+       'η' :   8,
+       'θ' :   9,
+       'ι' :  10,
+       'κ' :  20,
+       'λ' :  30,
+       'μ' :  40,
+       'ν' :  50,
+       'ξ' :  60,
+       'ο' :  70,
+       'π' :  80,
+       'ρ' : 100,
+       'ς' : 200,
+       'σ' : 200,
+       'τ' : 300,
+       'υ' : 400,
+       'φ' : 500,
+       'χ' : 600,
+       'ψ' : 700,
+       'ω' : 800,
+       'ϋ' : 400,
+       'Ϟ' :  90,
+       'ϟ' :  90,
+       'Ϡ' : 900,
+       'ϡ' : 900,
+       'ἀ' :   1,
+       'ἐ' :   5,
+       'ἡ' :   8,
+       'ἱ' :  10,
+       'Ἰ' :  10,
+       'Ὧ' : 800,
+       'ὰ' :   1,
+       'ά' :   1,
+       'ί' :  10,
+       'ὸ' :  70,
+       'ώ' : 800,
+       'ᾶ' :   1,
+       'ῆ' :   8,
+       'ῖ' :  10,
+       'ῦ' : 400,
+       'ῶ' : 800,
     }
 #_}
 
@@ -100,12 +173,22 @@ def words_and_letters_in_book(b): #_{
        print('  {:s}: {:5d}'.format(letter, letter_counter[letter]))
 #_}  
 
-def words_and_letters_in_books():
+def words_and_letters_in_books(): #_{
     words_and_letters_in_book('1mo')
     words_and_letters_in_book('2mo')
     words_and_letters_in_book('3mo')
     words_and_letters_in_book('4mo')
     words_and_letters_in_book('5mo')
+#_}
+
+def numeric_value_translation(word_de, lang): #_{
+
+    for r in wrd.execute('select word from strongs where lang = ? and word_de = ?', (lang, word_de)):
+        word = replace_nikkud(r[0])
+        return numeric_value(word)
+
+
+#_}
 
 def tests(): #_{
 
@@ -186,7 +269,25 @@ def tests(): #_{
                    
     #_}
 
+    def test_numeric_value_translation(): #_{
+
+        val_noah = numeric_value_translation('Noah', 'H')
+        if val_noah != 58:
+           raise Exception('Value of Noah: {:d}'.format(val_noah))
+        
+        print('ok Value Noah')
+
+        val_juda = numeric_value_translation('Juda', 'G')
+        if val_juda != 485:
+           raise Exception('Value of Juda: {:d}'.format(val_juda))
+        
+        print('ok Value Juda')
+
+
+    #_}
+
     test_1mo_1_1()
+    test_numeric_value_translation()
     letter_86()
     val_600000()
 
@@ -194,4 +295,4 @@ def tests(): #_{
 
 init()
 tests()
-words_and_letters_in_books()
+# words_and_letters_in_books()
